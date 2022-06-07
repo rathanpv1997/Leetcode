07fb9a8c400b14a32bac0fa1f0cd5d953256a004
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-08 00:38:33 +0530

    Time: 45 ms (71.86%), Space: 14.2 MB (9.66%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -7,12 +7,14 @@ class Solution:
         
         def dfs(i):
             if i >= len(nums):
-                res.append(cur.copy()) if cur not in res else res
+                res.append(cur.copy())
                 return
             
             cur.append(nums[i])
             dfs(i+1)
             cur.pop()
+            while i+1<len(nums) and nums[i] == nums[i+1]:
+                i += 1
             dfs(i+1)
         
         dfs(0)
