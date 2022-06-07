5e7e1a027105ed0a852c6c8875f50a43e6528564
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-07 23:41:06 +0530

    Time: 835 ms (68.86%), Space: 27.9 MB (77.67%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,9 +4,7 @@ class Solution:
         cur_sum = 0
         max_sub = nums[0]
         for i in range(n):
-            if cur_sum < 0:
-                cur_sum = nums[i]
-            else:
-                cur_sum = cur_sum + nums[i]
+            cur_sum = max(nums[i],cur_sum+nums[i])
             max_sub = max(cur_sum,max_sub)
         return max_sub
+    
