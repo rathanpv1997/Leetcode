a71e4d023914fadadddd156eb4d311bc40d23797
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-08 11:31:38 +0530

    Time: 209 ms (14.12%), Space: 17.1 MB (88.47%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,9 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
         for i in range(len(strs)):
-            if ''.join(sorted(list(strs[i]))) in d:
-                d[''.join(sorted(list(strs[i])))].append(strs[i])
+            if ''.join(sorted(strs[i])) in d:
+                d[''.join(sorted(strs[i]))].append(strs[i])
             else:
-                d[''.join(sorted(list(strs[i])))] = [strs[i]]
+                d[''.join(sorted(strs[i]))] = [strs[i]]
         return [value for value in d.values()]
             
