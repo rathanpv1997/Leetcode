5a3fc815a49ac576d7fce492a8153a3a2616bee6
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-09 14:23:04 +0530

    Time: 310 ms (88.88%), Space: 16.6 MB (40.95%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,53 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        r = len(grid)
+        c = len(grid[0])
+        visited = [[False for _ in range(c)] for _ in range(r)]
+        
+        def dfs(i,j):
+            if i < 0 or j < 0 or i > r-1 or j > c-1 or grid[i][j] == '0' or visited[i][j]:
+                return
+            visited[i][j] = True
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        
+        count = 0
+        for i in range(r):
+            for j in range(c):
+                if not visited[i][j] and grid[i][j] == '1':
+                    dfs(i,j)
+                    count += 1
+        return count
+    
+#     def numIslands(self, grid):
+#         """
+#         :type grid: List[List[str]]
+#         :rtype: int
+#         """
+#         if not grid: return 0
+#         r, c = len(grid), len(grid[0])
+#         visited = [[False for _ in range(c)] for _ in range(r)]
+
+#         def dfs(i, j):
+#             if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] == '0' or visited[i][j]:
+#                 return
+#             visited[i][j] = True
+#             dfs(i + 1, j)
+#             dfs(i - 1, j)
+#             dfs(i, j + 1)
+#             dfs(i, j - 1)
+
+#         count = 0
+#         for i in range(r):
+#             for j in range(c):
+#                 if not visited[i][j] and grid[i][j] == '1':
+#                     dfs(i, j)
+#                     count += 1
+#         return count
+                
+        
+                
