a5875e659e566658918f56b2d6279a0f93cd222f
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-20 00:36:58 +0530

    Time: 228 ms (97.05%), Space: 15.2 MB (7.71%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -3,14 +3,23 @@ from sortedcontainers import SortedList
 class MyCalendar:
 
     def __init__(self):
-        self.calendar = SortedList()
+        # self.calendar = SortedList()
+        self.calendar = []
 
     def book(self, start: int, end: int) -> bool:
-        start_index = SortedList.bisect_right(self.calendar,start)
-        end_index = SortedList.bisect_left(self.calendar,end)
+        # start_index = SortedList.bisect_right(self.calendar,start)
+        # end_index = SortedList.bisect_left(self.calendar,end)
+        # if start_index == end_index and start_index % 2 == 0:
+        #     self.calendar.add(end)
+        #     self.calendar.add(start)
+        #     return True
+        # return False
+        
+        start_index = bisect_right(self.calendar,start)
+        end_index = bisect_left(self.calendar,end)
         if start_index == end_index and start_index % 2 == 0:
-            self.calendar.add(end)
-            self.calendar.add(start)
+            insort_left(self.calendar,end)
+            insort_right(self.calendar,start)
             return True
         return False
 
