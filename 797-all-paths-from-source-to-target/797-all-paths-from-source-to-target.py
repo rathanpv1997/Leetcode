b2ca3ac2e9e23127ef162169d68ee8ea6ec33809
rathanpv1997 <rathanpv1997@gmail.com>
2022-06-21 23:06:35 +0530

    Time: 157 ms (37.33%), Space: 15.6 MB (96.87%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -4,17 +4,14 @@ class Solution:
         n = len(graph)
         
         def dfs(cur,i):
-            if cur[-1] == n-1:
+            # if cur[-1] == n-1:
+            #     res.append(cur[:])
+            #     return
+            if i == n-1:
                 res.append(cur[:])
                 return
-            if i == n-1:
-                return
-            for j in range(len(graph[i])):
-                # print(graph[i][j])
-                cur.append(graph[i][j])
-                # print(cur,graph[i][j],res)
-                dfs(cur,graph[i][j])
-                cur.pop()
+            for j in graph[i]:
+                dfs(cur + [j], j)
                 
         dfs([0],0)
         return res
